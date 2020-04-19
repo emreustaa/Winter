136dd5feb7a5b3d7b1695fdc6b92f00f36a3dca9
Hasan Kağıt <hasankagit1961@gmail.com>
2020-04-19 16:16:39 +0300

    one commit

---
+++ aglar_proje/src/aglar_proje/muzakere.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aglar_proje;
+
+/**
+ *
+ * @author hasan
+ */
+public class muzakere {
+    
+}
